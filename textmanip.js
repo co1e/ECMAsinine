e9e6f665c78b8b1ef4ef14cb9127808beef557f1
Thomas Cole <thmcole@gmail.com>
2013-07-31 23:11:11 -0400

    Update textmanip.js

--- textmanip.js
+++ textmanip.js
@@ -1,9 +1,20 @@
-var findAndRemoveTwoCharacter = function(s, c1, c2) {
+var findAndRemoveTwoCharacters = function(s, c1, c2) {
     if (s.indexOf(c1) !== -1) {
         var nS = s.substring(0, s.indexOf(c1));     
     }
     else if (s.indexOf(c2) !== -1) {
         var nS = s.substring(0, s.indexOf(c2));        
+    }
+    return nS;
+}
+
+// Alternate Style
+function findAndRemoveTwoCharacters(s, c1, c2) {
+    if (s.indexOf(c1) !== -1) {
+        var nS = s.substring(0, s.indexOf(c1));
+    }
+    else if (s.indexOf(c2) !== -1) {
+        var nS = s.substring(0, s.indexOf(c2));
     }
     return nS;
 }
